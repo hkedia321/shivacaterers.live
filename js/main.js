e7e90c48ba95abca984636d60343752a9713f28d
Harshit Kedia <harshitkedia32@gmail.com>
2021-02-13 23:56:08 +0530

    add og tags

--- js/main.js
+++ js/main.js
@@ -44,108 +44,108 @@
 /*----------------------------------------
     Slider Carousel
 ---------------------------------------- */
-    $(".slider-wrapper").owlCarousel({
-        loop:true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        smartSpeed: 2500,
-        items:1,
-        nav:true,
-        navText: ["<i class='zmdi zmdi-chevron-left'></i>","<i class='zmdi zmdi-chevron-right'></i>"],
-        dots:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    });
+    // $(".slider-wrapper").owlCarousel({
+    //     loop:true,
+    //     animateOut: 'fadeOut',
+    //     animateIn: 'fadeIn',
+    //     smartSpeed: 2500,
+    //     items:1,
+    //     nav:true,
+    //     navText: ["<i class='zmdi zmdi-chevron-left'></i>","<i class='zmdi zmdi-chevron-right'></i>"],
+    //     dots:true,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         600:{
+    //             items:1
+    //         },
+    //         1000:{
+    //             items:1
+    //         }
+    //     }
+    // });
     
 /*-------------------------------------
     Event Carousel
 -------------------------------------- */
-    $(".event-owl").owlCarousel({
-        loop:true,
-        items:1,
-        dots: true,
-		nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            480:{
-                items:1
-            },
-            768:{
-                items:2
-            }, 
-            992:{
-                items:3
-            }, 
-            1200:{
-                items:3
-            }
-		}
-    }); 
+    // $(".event-owl").owlCarousel({
+    //     loop:true,
+    //     items:1,
+    //     dots: true,
+	// 	nav:false,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         480:{
+    //             items:1
+    //         },
+    //         768:{
+    //             items:2
+    //         }, 
+    //         992:{
+    //             items:3
+    //         }, 
+    //         1200:{
+    //             items:3
+    //         }
+	// 	}
+    // }); 
     
 /*-------------------------------------
     Menu Carousel
 -------------------------------------- */
-    $(".menu-owl").owlCarousel({
-        loop:true,
-        items:1,
-        dots: true,
-		nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            480:{
-                items:1
-            },
-            768:{
-                items:2
-            }, 
-            992:{
-                items:3
-            }, 
-            1200:{
-                items:3
-            }
-		}
-    });    
+    // $(".menu-owl").owlCarousel({
+    //     loop:true,
+    //     items:1,
+    //     dots: true,
+	// 	nav:false,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         480:{
+    //             items:1
+    //         },
+    //         768:{
+    //             items:2
+    //         }, 
+    //         992:{
+    //             items:3
+    //         }, 
+    //         1200:{
+    //             items:3
+    //         }
+	// 	}
+    // });    
     
 /*-------------------------------------
     Testimonial Carousel
 -------------------------------------- */
-    $(".testimonial-owl").owlCarousel({
-        loop:true,
-        items:1,
-        dots: true,
-		nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            480:{
-                items:1
-            },
-            768:{
-                items:1
-            }, 
-            992:{
-                items:1
-            }, 
-            1200:{
-                items:1
-            }
-		}
-    });
+    // $(".testimonial-owl").owlCarousel({
+    //     loop:true,
+    //     items:1,
+    //     dots: true,
+	// 	nav:false,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         480:{
+    //             items:1
+    //         },
+    //         768:{
+    //             items:1
+    //         }, 
+    //         992:{
+    //             items:1
+    //         }, 
+    //         1200:{
+    //             items:1
+    //         }
+	// 	}
+    // });
 
 /*-------------------------------------------
     03. jQuery MeanMenu
